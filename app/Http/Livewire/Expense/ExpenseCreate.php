690d6859c84235e67ddf8362e4852366cc26d049
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-12-05 14:27:04 -0300

    Aula 25, Upload de imagem na criação do expense

--- app/Http/Livewire/Expense/ExpenseCreate.php
+++ app/Http/Livewire/Expense/ExpenseCreate.php
@@ -4,25 +4,34 @@ namespace App\Http\Livewire\Expense;
 
 use App\Models\Expense;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ExpenseCreate extends Component
 {
-    public $description, $amount, $type;
+    use WithFileUploads;
+
+    public $description, $amount, $type, $photo;
 
     protected $rules = [
         'description' => 'required',
         'amount' => 'required',
-        'type' => 'required'
+        'type' => 'required',
+        'photo' => 'image|nullable'
     ];
 
     public function createExpense()
     {
         $this->validate();
 
+        if ($this->photo) {
+            $photo = $this->photo->store('expenses-photos', 'public');
+        }
+
         auth()->user()->expenses()->create([
             'description' => $this->description,
             'amount' => $this->amount,
-            'type' => $this->type
+            'type' => $this->type,
+            'photo' => $photo??null
         ]);
 
         session()->flash('message', 'Registro criado com sucesso!');
